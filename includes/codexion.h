26fd8a530118f6a3570fa4ea8f3e32ac59de49a5
Daniel Rakotovao Ralaitsianatao <drakotov@3-B-5.42heilbronn.de>
2026-08-16 15:43:36 +0200

    setup all the files

---
+++ includes/codexion.h
@@ -0,0 +1,17 @@
+#ifndef CODEXION_H
+# define CODEXION_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <pthread.h>
+# include <sys/time.h>
+# include <unistd.h>
+
+
+
+
+
+
+
+#endif
