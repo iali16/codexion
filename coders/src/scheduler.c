6ed9b3098087d54cdb5e8cd92ef2cfc31af8c1db
Daniel Rakotovao Ralaitsianatao <drakotov@3-B-5.42heilbronn.de>
2026-09-12 02:59:55 +0200

    coder_id check

--- coders/src/scheduler.c
+++ coders/src/scheduler.c
@@ -6,7 +6,7 @@
 /*   By: drakotov <drakotov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/19 01:47:32 by drakotov          #+#    #+#             */
-/*   Updated: 2026/09/11 22:45:41 by drakotov         ###   ########.fr       */
+/*   Updated: 2026/09/12 02:15:40 by drakotov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	waiter_less(t_sched sched, t_waiter *a, t_waiter *b)
 	{
 		if (a->deadline != b->deadline)
 			return (a->deadline < b->deadline);
-		return (a->seq < b->seq);
+		return (a->coder_id < b->coder_id);
 	}
 	return (a->seq < b->seq);
 }
